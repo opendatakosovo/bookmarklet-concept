8894be85d40ecc0065628f081ccd4313621b1aaf
egzontina <egzontina.krasniqi@gmail.com>
2016-05-04 15:32:12 +0200

    bookmarklet logic is implemented,jquery file is added, some other minor fix

---
+++ js/bookmarklet.js
@@ -0,0 +1,44 @@
+
+function addCSS(url){
+  var headID = document.getElementsByTagName("head")[0];
+  var cssNode = document.createElement('link');
+  cssNode.type = 'text/css';
+  cssNode.rel = 'stylesheet';
+  cssNode.href = url;
+  cssNode.media = 'screen';
+  headID.appendChild(cssNode);
+}
+
+function toggleItem(id){
+  var item = document.getElementById(id);
+  if(item){
+    if ( item.style.display == "none"){
+      item.style.display = "";
+    }
+    else{
+      item.style.display = "none";
+    } 
+  }
+}
+
+(function(){
+
+  var iframe_url = "bookmarklet-form.html";
+  addCSS("css/custom-css.css");
+ 
+  var div = document.createElement("div");
+  div.id = "google_sheets_bookmarklet_id";
+  
+  var str = "";
+  str += "<table id='google_sheets_bookmarklet_table' valign='top' width='570' cellspacing='0' cellpadding='0'><tr><td width ='550' height='80'>";
+  str += "<iframe frameborder='0' scrolling='no' name='google_sheets_bookmarklet_iframe' id='google_sheets_bookmarklet_iframe' src='" + iframe_url + "' width='550px' height='275px' style='textalign:right; backgroundColor: white;'></iframe>";
+  str += "</td><td onClick='toggleItem(\"google_sheets_bookmarklet_id\");' title='click to close window' valign='top' align='center' width='20px'>";
+  str += "<a href='javascript:void(0);' style='width:100%; text-align: middle; color: #FF0000; font-family: Arial;'>x</a>";
+  str += "</td></tr></table>";
+  
+  div.innerHTML = str;
+  
+
+  document.body.insertBefore(div, document.body.firstChild);
+})()
+
--- js/google-sheet.js
+++ js/google-sheet.js
@@ -18,8 +18,7 @@ $("#form").submit(function(event){
     var serializedData = $form.serialize();
     
     // Let's disable the inputs for the duration of the Ajax request.
-    // Note: we disable elements AFTER the form data has been serialized.
-    // Disabled form elements will not be serialized.
+    // Note: we disable elements AFTER the form data has been serialized. Disabled form elements will not be serialized.
     $inputs.prop("disabled", true);
 
     // Fire off the request to /form.php
@@ -31,25 +30,16 @@ $("#form").submit(function(event){
 
     // Callback handler that will be called on success
     request.done(function (response, textStatus, jqXHR){
-        // Log a message to the console
-        console.log("Hooray, it worked!");
-        console.log(response);
-        console.log(textStatus);
-        console.log(jqXHR);
+        // console.log(textStatus);
     });
 
     // Callback handler that will be called on failure
     request.fail(function (jqXHR, textStatus, errorThrown){
-        // Log the error to the console
-        console.error(
-            "The following error occurred: "+ textStatus, errorThrown
-        );
+        console.error("The following error occurred: "+ textStatus, errorThrown);
     });
 
-    // Callback handler that will be called regardless
-    // if the request failed or succeeded
+    // Reenable the inputs, Callback handler that will be called regardless if the request failed or succeeded
     request.always(function () {
-        // Reenable the inputs
         $inputs.prop("disabled", false);
     });
 
